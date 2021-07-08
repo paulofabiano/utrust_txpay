25f4f041d8ef85a7104b9a35e71b685880fd0a46
Paulo Langer <paulo.langer@xing.com>
2021-07-08 23:56:18 +0100

    Checking transaction right after creating payment

--- lib/utrust_txpay_web/live/payments_live.ex
+++ lib/utrust_txpay_web/live/payments_live.ex
@@ -3,6 +3,7 @@ defmodule UtrustTxpayWeb.PaymentsLive do
 
   alias UtrustTxpay.Payments
   alias UtrustTxpay.Payments.Payment
+  alias UtrustTxpay.Etherscan.{Scraper, Transaction}
 
   alias UtrustTxpayWeb.{FormComponent, PaymentsListComponent}
 
@@ -17,19 +18,81 @@ defmodule UtrustTxpayWeb.PaymentsLive do
     socket =
       assign(socket,
         payments: list_payments,
-        changeset: changeset
+        changeset: changeset,
+        checking: nil
       )
 
     {:ok, socket, temporary_assigns: [payments: []]}
   end
 
   @impl true
+  def handle_event("create-payment", %{"payment" => params}, socket) do
+    case Payments.create_payment(params) do
+      {:ok, payment} ->
+        socket =
+          update(
+            socket,
+            :payments,
+            fn payments -> [payment | payments] end
+          )
+
+        changeset = Payments.change_payment(%Payment{})
+
+        socket = assign(socket, changeset: changeset, checking: payment.id)
+
+        {:noreply, socket}
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        socket = assign(socket, changeset: changeset)
+
+        {:noreply, socket}
+    end
+  end
+
+  @impl true
+  def handle_event("verify-payment", %{"id" => id}, socket) do
+    :timer.sleep(500)
+
+    case Payments.get_payment!(id) do
+      %Payment{} = payment ->
+        case Scraper.get_transaction(payment.hash) do
+          %Transaction{} = transaction ->
+            update_payment_status(payment, transaction.block_confirmations)
+        end
+    end
+
+    {:noreply, socket}
+  end
+
+  defp update_payment_status(payment, block_confirmations) when block_confirmations >= 2 do
+    Payments.update_payment(payment, %{
+      status: "confirmed",
+      block_confirmations: Integer.to_string(block_confirmations)
+    })
+  end
+
+  defp update_payment_status(payment, block_confirmations) do
+    Payments.update_payment(payment, %{
+      block_confirmations: Integer.to_string(block_confirmations)
+    })
+  end
+
+  @impl true
   def handle_info({:payment_created, payment}, socket) do
+    new_payment =
+      case Scraper.get_transaction(payment.hash) do
+        %Transaction{} = transaction ->
+          update_payment_status(payment, transaction.block_confirmations)
+
+        _ ->
+          payment
+      end
+
     socket =
       update(
         socket,
         :payments,
-        fn payments -> [payment | payments] end
+        fn payments -> [new_payment | payments] end
       )
 
     {:noreply, socket}
